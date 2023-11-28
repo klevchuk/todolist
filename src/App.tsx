f3d81edb23553bb81838ee3cb524399b3a764869
klevchuk <lemoniklava@gmail.com>
2023-11-28 21:13:47 +0800

    deleted tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
-let arr = [ []], ()
 
 function App() {
-    let tasks = [
+
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS/TS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "Redux", isDone: false}
-    ]
-
-    useState(tasks);
+    ]);
 
     function removeTask(id: number) {
-        tasks = tasks.filter(t => t.id !== id)
+       let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks);
     }
 
 
